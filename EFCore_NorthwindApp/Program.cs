f982b10ae9b283145acbc8658e0e738758369ab4
kangdiancp <kangdiancp@gmail.com>
2022-04-08 08:56:16 +0700

    init ef

---
+++ EFCore_NorthwindApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using EFCore_NortwindDb;
+
+
+namespace EFCore_NorthwindApp
+{
+    class Program
+    {
+        private static IConfigurationRoot Configuration;
+        private static DbContextOptionsBuilder<NorthwindContext> optionsBuilder;
+        static void Main(string[] args)
+        {
+            BuildConfiguration();
+            Console.WriteLine($"ConnectionString : {Configuration.GetConnectionString("NorthWindDS")}");
+            BuildOptions();
+            
+
+            //ListCustomer();
+
+
+        }
+
+        static void BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        static void BuildOptions()
+        {
+            optionsBuilder = new DbContextOptionsBuilder<NorthwindContext>();
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("NorthWindDS"));
+        }
+
+        static void ListCustomer()
+        {
+            using (var db = new NorthwindContext(optionsBuilder.Options))
+            {
+                var customers = db.Customers.OrderByDescending(x => x.CompanyName).Take(10).ToList();
+
+                foreach (var cust in customers)
+                {
+                    Console.WriteLine($"{cust.CompanyName} {cust.ContactName}");
+                }
+            }
+        }
+
+
+        
+
+
+    }
+}
